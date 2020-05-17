9fe54b0072d93562fecfe1dae8c0cfa1f7f97d0d
Eli <sporial@gmail.com>
2020-05-18 00:32:51 +0800

    Basic enemy AI and animations + placeholder [PH] text
    Hooked up *extremely* basic ai to make enemies approach player and animate. Currently enemies face the wrong direction, when moving. Added some placeholder directional text for testrun build.

--- Warrior Chef Game Unity Project/Assets/Scripts/enemyController.cs
+++ Warrior Chef Game Unity Project/Assets/Scripts/enemyController.cs
@@ -6,7 +6,45 @@ public class enemyController : MonoBehaviour
 {
     public int health = 20;
 
+    public float speed = 10f;
+
     private Rigidbody2D rb;
+
+    public Animator animator;
+
+    public Transform target;
+    public float maxDistance = 1;
+    public float minDistance = 1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Vector2 vel = rb.velocity;
+        //float xVel = vel.x;
+        //animator.SetFloat("Speed", Mathf.Abs(xVel));
+        transform.LookAt(target.position);
+         transform.Rotate(new Vector3(0,-90,0),Space.Self);
+        //move towards the player
+         if (Vector3.Distance(transform.position, target.position) >= minDistance)
+         {  
+            transform.Translate(new Vector3(speed * Time.deltaTime,0,0) );
+            animator.SetFloat("Speed", Mathf.Abs(speed * Time.deltaTime));
+         }    
+            //shoots toward player every so often
+            if (Vector3.Distance(transform.position, target.position) <= maxDistance)
+            {  
+                Attack();
+            }
+    }
+
+     public void Attack()
+   {
+       animator.SetTrigger("Attack");
+   }
 
     
 
