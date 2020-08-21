e169c468edf94d98bc391501fa3132c51bb0c605
Kyle Lannen <kylelannen2@gmail.com>
2020-08-21 21:12:11 +0800

    Level gen + prototype map stuff
    Testing world generates (simple script)
    Fade to black when ending level and opening new level
    Prototype map
    (Rough) prototype for town

---
+++ Warrior Chef Game Unity Project/Assets/Scripts/CookingButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CookingButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GameObject.Find("LevelController").GetComponent<FadeControls>().StartFade();
+
+        GameObject.Find("PlayerUI");
+        GameObject.Find("PlayerUI").GetComponent<cookingMinigameController>().YesChef();
+    }
+}
