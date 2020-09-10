3155dbbc5377512a0339578a6b46f41dc0ed40e4
Eli <sporial@gmail.com>
2020-09-10 23:21:12 +0800

    Modified enemyController, added StateMachines for enemy behaviours
    The enemyController no longer controls enemy movement. This is now down through StateMachines on the animations. This allows enemies to have behaviours. Raycasts on the enemyController create a 'Line of sight' that if the player steps into the enemies will change from idle behaviour to an attacking behaviour. Enemies now need a groundDetector transform im their prefab, that allows them to avoid falling off ledges and bumping into walls/each other. Additionally, Player needs to be tagged with "Player" Enemies with "Enemy" and terrain as "Ground".  Also introduced combo animations, although they are yet unused.

---
+++ Warrior Chef Game Unity Project/Assets/Scripts/followBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followBehaviour : StateMachineBehaviour
+{
+    private Transform playerPos;
+    public float speed;
+    enemyController enemy;
+    //this is a statemachine that is attached to the enraged animation - it now handles movement toward the player.
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        enemy = animator.GetComponent<enemyController>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy.LookAtPlayer();
+
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
